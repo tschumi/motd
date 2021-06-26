e70ff9a2f7cd8f6368fc516c25e9002f06ed3ab9
Daniel Tschumi <daniel.tschumi@gmx.com>
2021-06-26 13:04:58 +0200

    Add a build script to build executables

---
+++ build.py
@@ -0,0 +1,30 @@
+"""
+This script will build the executables.
+
+Usage: python pyinstaller.py
+
+"""
+import platform
+import PyInstaller.__main__
+
+os = platform.system()
+
+if os == "Darwin":
+    PyInstaller.__main__.run([
+        'src/backend/main.py',
+        '--name=motd',
+        '--icon=src/assets/icons/mac/icon.icns',
+        '--onefile',
+        '--windowed',
+        '--add-data=gui:gui'
+    ])
+
+if os == "Windows":
+        PyInstaller.__main__.run([
+            'src/backend/main.py',
+            '--name=motd',
+            '--icon=src/assets/icons/win/icon.ico',
+            '--onefile',
+            '--windowed',
+            '--add-data=gui:gui'
+        ])
